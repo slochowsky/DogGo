60dc061d8b7cc806537a4869feefea524f09c52d
blake slochowsky <blakeslochowsky.com>
2020-06-17 09:49:28 -0500

    dog has gone go

---
+++ DogGo1/Controllers/OwnersController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DogGo1.Models;
+using DogGo1.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace DogGo1.Controllers
+{
+    public class OwnersController : Controller
+    {
+        private readonly OwnerRepository _ownerRepo;
+        private readonly DogRepository _dogRepo;
+
+
+        // The constructor accepts an IConfiguration object as a parameter. This class comes from the ASP.NET framework and is useful for retrieving things out of the appsettings.json file like connection strings.
+        public OwnersController(IConfiguration config)
+        {
+            _ownerRepo = new OwnerRepository(config);
+            _dogRepo = new DogRepository(config);
+        }
+
+        // GET: OwnerController
+        public ActionResult Index()
+        {
+            List<Owner> owners = _ownerRepo.GetAllOwners();
+
+            return View(owners);
+        }
+
+        // GET: OwnerController/Details/5
+        public ActionResult Details(int id)
+        {
+            Owner owner = _ownerRepo.GetOwnerById(id);
+            owner.Dogs = _dogRepo.GetDogsByOwner(id);
+
+            if (owner == null)
+            {
+                return NotFound();
+            }
+
+            return View(owner);
+        }
+
+        // GET: OwnerController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: OwnerController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Owner owner)
+        {
+            try
+            {
+                _ownerRepo.AddOwner(owner);
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception Ex)
+            {
+                return View(owner);
+            }
+        }
+
+        // GET: OwnerController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Owner owner = _ownerRepo.GetOwnerById(id);
+
+            if (owner == null)
+            {
+                return NotFound();
+            }
+
+            return View(owner);
+        }
+
+        // POST: OwnerController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Owner owner)
+        {
+            try
+            {
+                _ownerRepo.UpdateOwner(owner);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(owner);
+            }
+        }
+
+        // GET: Owners/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Owner owner = _ownerRepo.GetOwnerById(id);
+
+            return View(owner);
+        }
+
+        // POST: Owners/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Owner owner)
+        {
+            try
+            {
+                _ownerRepo.DeleteOwner(id);
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                return View(owner);
+            }
+        }
+    }
+}
---
+++ DogGo1/Repositories/DogRepository.cs
@@ -0,0 +1,207 @@
+﻿using DogGo1.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogGo1.Repositories
+{
+    public class DogRepository
+    {
+        private readonly IConfiguration _config;
+
+        // The constructor accepts an IConfiguration object as a parameter. This class comes from the ASP.NET framework and is useful for retrieving things out of the appsettings.json file like connection strings.
+        public DogRepository(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        public List<Dog> GetAllDogs()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT d.Id AS dId, d.[Name] AS dName, OwnerId, Breed, Notes, ImageUrl,
+                            Email, o.[Name] AS oName, Address, Phone, NeighborhoodId, n.[Name] AS nName                           
+                        FROM Dog d
+                        JOIN Owner o ON o.id = d.OwnerId
+                        JOIN Neighborhood n ON n.Id = o.NeighborhoodId
+                    ";
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    List<Dog> dogs = new List<Dog>();
+                    while (reader.Read())
+                    {
+                        string notes = "";
+                        string imageUrl = "";
+                        if (!reader.IsDBNull(reader.GetOrdinal("Notes")))
+                        {
+                            notes = reader.GetString(reader.GetOrdinal("Notes"));
+                        }
+                        if (!reader.IsDBNull(reader.GetOrdinal("ImageUrl")))
+                        {
+                            imageUrl = reader.GetString(reader.GetOrdinal("ImageUrl"));
+                        }
+                        Dog dog = new Dog
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("dId")),
+                            Name = reader.GetString(reader.GetOrdinal("dName")),
+                            Breed = reader.GetString(reader.GetOrdinal("Breed")),
+                            Notes = notes,
+                            ImageUrl = imageUrl,
+                            OwnerId = reader.GetInt32(reader.GetOrdinal("OwnerId")),
+                            Owner = new Owner
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("OwnerId")),
+                                Name = reader.GetString(reader.GetOrdinal("oName")),
+                                Address = reader.GetString(reader.GetOrdinal("Address")),
+                                PhoneNumber = reader.GetString(reader.GetOrdinal("Phone")),
+                                NeighborhoodId = reader.GetInt32(reader.GetOrdinal("NeighborhoodId")),
+                                Neighborhood = new Neighborhood
+                                {
+                                    Id = reader.GetInt32(reader.GetOrdinal("NeighborhoodId")),
+                                    Name = reader.GetString(reader.GetOrdinal("nName"))
+                                }
+                            }
+                        };
+
+                        dogs.Add(dog);
+                    }
+
+                    reader.Close();
+
+                    return dogs;
+                }
+            }
+        }
+
+        public List<Dog> GetDogsByOwner(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT Id, [Name], OwnerId, Breed, Notes, ImageUrl
+                        FROM Dog
+                        WHERE OwnerId = @id
+                    ";
+
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    List<Dog> dogs = new List<Dog>();
+
+                    while (reader.Read())
+                    {
+                        string notes = "";
+                        string imageUrl = "";
+                        if (!reader.IsDBNull(reader.GetOrdinal("Notes")))
+                        {
+                            notes = reader.GetString(reader.GetOrdinal("Notes"));
+                        }
+                        if (!reader.IsDBNull(reader.GetOrdinal("ImageUrl")))
+                        {
+                            imageUrl = reader.GetString(reader.GetOrdinal("ImageUrl"));
+                        }
+                        Dog dog = new Dog
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                            Breed = reader.GetString(reader.GetOrdinal("Breed")),
+                            Notes = notes,
+                            ImageUrl = imageUrl
+                        };
+
+                        dogs.Add(dog);
+                    }
+                    reader.Close();
+                    return dogs;
+                }
+            }
+        }
+
+        public Dog GetDogById(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT d.Id AS dId, d.[Name] AS dName, OwnerId, Breed, Notes, ImageUrl,
+                            Email, o.[Name] AS oName, Address, Phone, NeighborhoodId, n.[Name] AS nName                           
+                        FROM Dog d
+                        JOIN Owner o ON o.id = d.OwnerId
+                        JOIN Neighborhood n ON n.Id = o.NeighborhoodId
+                        WHERE d.Id = @id";
+
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        string notes = "";
+                        string imageUrl = "";
+                        if (!reader.IsDBNull(reader.GetOrdinal("Notes")))
+                        {
+                            notes = reader.GetString(reader.GetOrdinal("Notes"));
+                        }
+                        if (!reader.IsDBNull(reader.GetOrdinal("ImageUrl")))
+                        {
+                            imageUrl = reader.GetString(reader.GetOrdinal("ImageUrl"));
+                        }
+                        Dog dog = new Dog
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("dId")),
+                            Name = reader.GetString(reader.GetOrdinal("dName")),
+                            Breed = reader.GetString(reader.GetOrdinal("Breed")),
+                            Notes = notes,
+                            ImageUrl = imageUrl,
+                            OwnerId = reader.GetInt32(reader.GetOrdinal("OwnerId")),
+                            Owner = new Owner
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("OwnerId")),
+                                Name = reader.GetString(reader.GetOrdinal("oName")),
+                                Address = reader.GetString(reader.GetOrdinal("Address")),
+                                PhoneNumber = reader.GetString(reader.GetOrdinal("Phone")),
+                                NeighborhoodId = reader.GetInt32(reader.GetOrdinal("NeighborhoodId")),
+                                Neighborhood = new Neighborhood
+                                {
+                                    Id = reader.GetInt32(reader.GetOrdinal("NeighborhoodId")),
+                                    Name = reader.GetString(reader.GetOrdinal("nName"))
+                                }
+                            }
+                        };
+
+                        reader.Close();
+                        return dog;
+                    }
+                    else
+                    {
+                        reader.Close();
+                        return null;
+                    }
+                }
+            }
+        }
+    }
+}
